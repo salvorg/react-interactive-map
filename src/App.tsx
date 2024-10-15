85a6d27f621ab0c74060ceb3cb7d2ed755f308fe
Almaz Balbaev <kujitengu@gmail.com>
2024-10-15 22:45:41 +0600

    added routing logic with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import Map from "./components/Map";
+import Map from "./pages/Map";
+import {Route, Routes} from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Map />
-    </div>
+    <>
+      <Routes>
+        path="/pscenters"
+        <Route
+          index
+          element={
+            <>
+            </>
+          }
+        />
+        <Route
+          path="/map"
+          element={
+            <>
+              <Map />
+            </>
+          }
+        />
+        {/*<Route*/}
+        {/*  path="/test"*/}
+        {/*  element={*/}
+        {/*    <>*/}
+        {/*      <PageTitle title="Testing"/>*/}
+        {/*      <Test/>*/}
+        {/*    </>*/}
+        {/*  }*/}
+        {/*/>*/}
+      </Routes>
+    </>
   );
 }
 
