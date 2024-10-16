36a7260db3ed62b9ce6de8aee3431cd55a9278ef
Almaz Balbaev <kujitengu@gmail.com>
2024-10-17 03:36:36 +0600

    created Footer.tsx

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-burgundy text-white p-4">
+      <div className="max-w-7xl w-full mx-auto p-6">
+        <h1>Footer</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
