094e80c5ce7ff1dcd8cf7260145de5abfa093e06
Almaz Balbaev <kujitengu@gmail.com>
2024-10-17 03:35:38 +0600

    created Header with logo

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+
+  return (
+    <div className="bg-white text-white p-4">
+      <div className="max-w-7xl w-full mx-auto sm:px-6">
+        <img src="/media-consult-logo.png" alt="Logo" className="h-10 mr-4" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+
+
