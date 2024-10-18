f14a60eb88f5e97c3d689daf5cbf1d0600aa1784
Almaz Balbaev <kujitengu@gmail.com>
2024-10-19 01:09:25 +0600

    created home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="p-10">
+      <div className="p-10">
+        Home
+      </div>
+      <div className="p-10">
+        Home
+      </div>
+      <div className="p-10">
+        Home
+      </div>
+      <div className="p-10">
+        Home
+      </div>
+      <div className="p-10">
+        Home
+      </div>
+      <div className="p-10">
+        Home
+      </div>
+      <div className="p-10">
+        Home
+      </div>
+      <div className="p-10">
+        Home
+      </div>
+      <div className="p-10">
+        Home
+      </div>
+      <div className="p-10">
+        Home
+      </div>
+      Home
+    </div>
+  );
+};
+
+export default Home;
