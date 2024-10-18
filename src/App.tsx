40b40a16dadf067b99301116352bcf3002112d0f
Almaz Balbaev <kujitengu@gmail.com>
2024-10-19 01:10:28 +0600

    created routing for home and sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,32 @@
 import React from 'react';
 import Map from "./pages/Map";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./layout/Layout";
+import About from "./pages/About";
+import News from "./pages/News";
+import Analytics from "./pages/Analytics";
+import MediaLaw from "./pages/MediaLaw";
+import Library from "./pages/Library";
+import Projects from "./pages/Projects";
+import Auth from "./pages/Auth";
+
 function App() {
   return (
-    <>
+    <div className="min-h-screen bg-gray-100 bg-white">
       <Routes>
-        path="/pscenters"
-        <Route
-          index
-          element={
-            <>
-            </>
-          }
-        />
-        <Route
-          path="/map"
-          element={
-            <>
-              <Map />
-            </>
-          }
-        />
-        {/*<Route*/}
-        {/*  path="/test"*/}
-        {/*  element={*/}
-        {/*    <>*/}
-        {/*      <PageTitle title="Testing"/>*/}
-        {/*      <Test/>*/}
-        {/*    </>*/}
-        {/*  }*/}
-        {/*/>*/}
+        <Route index element={<Layout><Home/></Layout>} />
+        <Route path="/about" element={<Layout><About/></Layout>} />
+        <Route path="/news" element={<Layout><News/></Layout>} />
+        <Route path="/analytics" element={<Layout><Analytics/></Layout>} />
+        <Route path="/media-law" element={<Layout><MediaLaw/></Layout>} />
+        <Route path="/library" element={<Layout><Library/></Layout>} />
+        <Route path="/projects" element={<Layout><Projects/></Layout>} />
+        <Route path="/sign-in" element={<Layout><Auth/></Layout>} />
+        <Route path="/sign-up" element={<Layout><Auth/></Layout>} />
+        <Route path="/map" element={<Map/>} />
       </Routes>
-    </>
+    </div>
   );
 }
 
